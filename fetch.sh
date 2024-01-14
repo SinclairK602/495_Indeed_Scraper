ab6c05135dc60ce2fb9dcf7e8be33bd33b25b288
Kyle Sinclair <ksinclair602@pm.me>
2024-01-14 16:49:08 -0700

    Modify query to pull meta-analysis and RCT studies as the previous query looking for 'trending' was searching for studies that contained such word. This fetch.sh will be used to automate the retrieval of Abstracts

---
+++ fetch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define a fixed date range
+yesterday_date=$(date -d "yesterday" +%Y/%m/%d)
+today=$(date +%Y/%m/%d)
+
+# Fetch meta-analysis abstracts within the fixed date range
+esearch -db pubmed -query "meta-analysis[Publication Type] AND ($yesterday_date[PDAT] : $today[PDAT])" | \
+efetch -format abstract > meta.txt
+
+# Fetch randomized controlled trial abstracts within the fixed date range
+esearch -db pubmed -query "\"randomized controlled trial\"[Publication Type] AND ($yesterday_date[PDAT] : $today[PDAT])" | \
+efetch -format abstract > rct.txt
